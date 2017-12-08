a40bc15995c2127de824bf461d6b15c2725439a1
Cameron McMillan <cmcmillan8@gatech.edu>
2017-12-07 17:59:59 -0800

    Working updateTargets

--- BasicCGLA/updateTargets.m
+++ BasicCGLA/updateTargets.m
@@ -1,21 +1,35 @@
 function uavs = updateTargets(uavs, targets, A)
-test = (cellfun(@(c) c.state.visited, targets));
-unVisited = targets{~test};
-numTargets = size(targets,2);
-numUavs = size(uavs, 2);
-n = sqrt(size(A, 1));
+unVisitedBool = (cellfun(@(c) c.state.visited, targets));
+unVisitedBool = ~unVisitedBool;
+unVisited = {targets{unVisitedBool}};
+numTargetsUnvis = size(unVisited,2);
 
-potentialCombs = zeros(numUavs, numTargets-1);
+activeBool = (cellfun(@(c) c.state.active, uavs));
+activeBool = logical(activeBool);
+activeUavs = {uavs{activeBool}};
+numUavs = size(activeUavs, 2);
+
+n = sqrt(size(A, 1));
+targList = 1:size(targets,2);
+uavList = 1:size(uavs,2);
+potentialCombs = zeros(numUavs, numTargetsUnvis-1);
 
 for i = 1:numUavs
-    for j = 2:numTargets
-        ind1 = sub2ind([n, n], uavs{i}.state.x, uavs{i}.state.y);
-        ind2 = sub2ind([n, n], targets{j}.state.x, targets{j}.state.y);
-        potentialCombs(i,j-1) = A(ind1,ind2);
+    for j = 1:numTargetsUnvis
+        ind1 = sub2ind([n, n], activeUavs{i}.state.x, activeUavs{i}.state.y);
+        ind2 = sub2ind([n, n], unVisited{j}.state.x, unVisited{j}.state.y);
+        potentialCombs(i,j) = A(ind1,ind2);
     end
     
 end
 tic
-[assignment, cost] = munkres(potentialCombs)
+[assignment, ~] = munkres(potentialCombs);
 toc
+assignment =assignment + 1;
+targList = [1 targList(unVisitedBool)];
+uavList(activeBool);
+
+for i = 1:numUavs
+    uavs{uavList(i)}.trait.target = targList(assignment(i));
+end
 end
